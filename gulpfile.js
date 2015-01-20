f88aef2a2d69dec8b5df553271d3f5a739838213
Robert Vogt <robert.vogt@namics.com>
2015-01-20 17:17:48 +0100

    Possible less caching solution

--- gulpfile.js
+++ gulpfile.js
@@ -9,29 +9,58 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     imagemin = require('gulp-imagemin'),
     cache = require('gulp-cached'),
-    debug = require('gulp-debug');
+    remember = require('gulp-remember'),
+    debug = require('gulp-debug'),
+    header = require('gulp-header'),
+    fs = require('fs');
 
-gulp.task('compile-less', function() {
+function getSourceFiles(ext) {
     var assets = require('./config.json').assets;
-
     for (var key in assets) {
-        if (assets.hasOwnProperty(key)) {
-            var asset = assets[key];
-            if ('.css' === path.extname(key)) {
-                gulp
-                    .src(asset)
-                    .pipe(cache('less-compile'))
-                    .pipe(debug())
-                    .pipe(plumber())
-                    .pipe(less())
-                    .pipe(minify())
-                    .pipe(concat(key))
-                    .pipe(
-                        gulp.dest('./public/latest/')
-                    );
+        if (assets.hasOwnProperty(key) && ext === path.extname(key)) {
+            var asset = assets[key],
+                result = {
+                    name: key,
+                    deps: [],
+                    src: []
+                };
+
+            for (var fkey in asset) {
+                var filepath = asset[fkey];
+                if (filepath.indexOf('+') === 0) {
+                    result.deps.push(filepath.substr(1));
+                } else {
+                    result.src.push(filepath);
+                }
             }
+
+            return result;
         }
     }
+
+    return [];
+}
+
+gulp.task('compile-less', function() {
+    var assets = getSourceFiles('.css'),
+        imports = '';
+
+    assets.deps.forEach(function(src) {
+        imports += fs.readFileSync(src)
+    });
+
+    return gulp
+        .src(assets.src)
+        .pipe(debug())
+        .pipe(header(imports))
+        .pipe(cache(assets.name))
+        .pipe(less())
+        .pipe(remember(assets.name))
+        .pipe(minify())
+        .pipe(concat(assets.name))
+        .pipe(
+            gulp.dest('public/latest/')
+        );
 });
 
 gulp.task('compile-js', function() {
@@ -43,9 +72,9 @@ gulp.task('compile-js', function() {
             if ('.js' === path.extname(key)) {
                 gulp
                     .src(asset)
-                    .pipe(cache('js-compile'))
                     .pipe(debug())
                     .pipe(plumber())
+                    .pipe(cache(key))
                     .pipe(jshint())
                     .pipe(jshint.reporter('jshint-stylish'))
                     .pipe(concat(key))
@@ -70,20 +99,30 @@ gulp.task('minify-img', function() {
         .pipe(gulp.dest('./assets/img'));
 });
 
-gulp.task('watch', ['compile-less', 'compile-js', 'minify-img'], function() {
-    watch(
-        ['./assets/**/*.css', './assets/**/*.less', './components/**/*.css', './components/**/*.less'],
-        function(files, cb) {
-            gulp.start('compile-less', cb);
+gulp.task('watch', ['compile-less'], function() {
+    var watcher = gulp.watch(['./assets/**/*.less', './components/**/*.less'], ['compile-less']);
+    watcher.on('change', function(e) {
+        if ('delete' === e.type) {
+            delete cache.caches.scripts[e.path];
+            remember.forget('scripts', e.path);
         }
-    );
-
-    watch(
-        ['./assets/**/*.js', './components/**/*.js'],
-        function(files, cb) {
-            gulp.start('compile-js', cb);
-        }
-    );
+    });
 });
+
+//gulp.task('watch', ['css', 'compile-js', 'minify-img'], function() {
+//    watch(
+//        ['./assets/**/*.css', './assets/**/*.less', './components/**/*.css', './components/**/*.less'],
+//        function(files, cb) {
+//            gulp.start('css', cb);
+//        }
+//    );
+//
+//    watch(
+//        ['./assets/**/*.js', './components/**/*.js'],
+//        function(files, cb) {
+//            gulp.start('compile-js', cb);
+//        }
+//    );
+//});
 
 gulp.task('default', ['compile-less', 'compile-js']);

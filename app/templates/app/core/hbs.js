d21bec36e1d4997a67bebe25f2239948f9b73445
Robert Vogt <robert.vogt@namics.com>
2015-02-09 17:39:25 +0100

    Move custom project helpers to project dir

--- app/templates/app/core/hbs.js
+++ app/templates/app/core/hbs.js
@@ -2,13 +2,15 @@ var fs = require('fs'),
     path = require('path'),
     hbs = require('hbs'),
     cfg = require('./config'),
-    helpersDir = path.normalize(__dirname + '../../helpers/');
+    helpersDir = cfg.sentinel.base_path + 'project/helpers/';
 
 hbs.registerPartials(cfg.sentinel.base_path + cfg.sentinel.view_partials_directory);
 
 var files = fs.readdirSync(helpersDir);
 files.forEach(function(file) {
-    var name = path.basename(helpersDir + file, '.js');
-    hbs.registerHelper(name, require(helpersDir + file));
+    if ('.js' === path.extname(file)) {
+        var name = path.basename(helpersDir + file, '.js');
+        hbs.registerHelper(name, require(helpersDir + file));
+    }
 });
 module.exports = hbs;
--- app/templates/sentinel.js
+++ app/templates/sentinel.js
@@ -2,9 +2,11 @@ var express = require('express'),
     app = express(),
     cfg = require('./app/core/config'),
     router = require('./app/core/router'),
-    hbs = require('./app/core/hbs');
+    hbs = require('./app/core/hbs'),
+    bodyParser = require('body-parser');
 
 app.use(router);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', cfg.sentinel.view_file_extension);
 app.set('views', cfg.sentinel.view_directory);
 app.engine(cfg.sentinel.view_file_extension, hbs.__express);

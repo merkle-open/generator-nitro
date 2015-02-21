829a2dac58813a5a4f8b12a3ad3173a405366e27
Robert Vogt <robert.vogt@namics.com>
2015-02-21 18:39:30 +0100

    Move project components to project dir

--- app/templates/app/core/hbs.js
+++ app/templates/app/core/hbs.js
@@ -1,16 +1,32 @@
 var fs = require('fs'),
-    path = require('path'),
-    hbs = require('hbs'),
-    cfg = require('./config'),
-    helpersDir = cfg.sentinel.base_path + 'project/helpers/';
+	path = require('path'),
+	hbs = require('hbs'),
+	cfg = require('./config'),
+	coreHelpersDir = cfg.sentinel.base_path + 'app/helpers/',
+	projectHelpersDir = cfg.sentinel.base_path + 'project/helpers/';
 
 hbs.registerPartials(cfg.sentinel.base_path + cfg.sentinel.view_partials_directory);
 
-var files = fs.readdirSync(helpersDir);
-files.forEach(function(file) {
-    if ('.js' === path.extname(file)) {
-        var name = path.basename(helpersDir + file, '.js');
-        hbs.registerHelper(name, require(helpersDir + file));
-    }
+var files = {},
+	coreFiles = fs.readdirSync(coreHelpersDir),
+	projectFiles = fs.readdirSync(projectHelpersDir);
+
+coreFiles.map(function(file) {
+	if ('.js' === path.extname(file)) {
+		files[path.basename(file, '.js')] = coreHelpersDir + file;
+	}
 });
+
+projectFiles.map(function(file) {
+	if ('.js' === path.extname(file)) {
+		files[path.basename(file, '.js')] = projectFiles + file;
+	}
+});
+
+for (var key in files) {
+	if (files.hasOwnProperty(key)) {
+		hbs.registerHelper(key, require(files[key]));
+	}
+}
+
 module.exports = hbs;
--- app/templates/app/helpers/component.js
+++ app/templates/app/helpers/component.js
@@ -1,41 +1,36 @@
 var fs = require('fs'),
-    hbs = require('hbs'),
-    path = require('path'),
-    cfg = require('../core/config.js');
-
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+	hbs = require('hbs'),
+	path = require('path'),
+	cfg = require('../core/config.js');
 
 module.exports = function(modName, variant) {
-    for (var key in cfg.sentinel.components) {
-        var component = cfg.sentinel.components[key];
-        if (component.hasOwnProperty('path')) {
-            var filename = modName.toLowerCase() + '.' + cfg.sentinel.view_file_extension;
-            ;
+	for (var key in cfg.sentinel.components) {
+		var component = cfg.sentinel.components[key];
+		if (component.hasOwnProperty('path')) {
+			var filename = modName.toLowerCase() + '.' + cfg.sentinel.view_file_extension;
 
-            if ('string' === typeof variant) {
-                filename = modName.toLowerCase() + '-' + variant.toLowerCase() + '.' + cfg.sentinel.view_file_extension;
-            }
+			if ('string' === typeof variant) {
+				filename = modName.toLowerCase() + '-' + variant.toLowerCase() + '.' + cfg.sentinel.view_file_extension;
+			}
 
-            var fullPath = path.join(
-                cfg.sentinel.base_path,
-                component.path,
-                '/',
-                capitalize(modName),
-                '/',
-                filename
-            );
+			var fullPath = path.join(
+				cfg.sentinel.base_path,
+				component.path,
+				'/',
+				modName,
+				'/',
+				filename
+			);
 
-            if (fs.existsSync(fullPath)) {
-                return new hbs.handlebars.SafeString(
-                    hbs.handlebars.compile(
-                        fs.readFileSync(fullPath, 'utf8')
-                    ).call()
-                );
-            }
-        }
-    }
+			if (fs.existsSync(fullPath)) {
+				return new hbs.handlebars.SafeString(
+					hbs.handlebars.compile(
+						fs.readFileSync(fullPath, 'utf8')
+					).call()
+				);
+			}
+		}
+	}
 
-    throw new Error('Component ' + modName + ' not found. (Hint: have you added it to config.json?)');
+	throw new Error('Component ' + modName + ' not found. (Hint: have you added it to config.json?)');
 };

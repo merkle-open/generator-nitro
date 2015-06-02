f8ffabf6f56e4fd8cbe21851b3be8ca2faa01fee
Christoph Buehler <christoph.buehler@namics.com>
2015-06-02 08:37:17 +0200

    update skins to new typescript way (export via compiler instead of Module[mod][skin] = ... call)

--- app/templates/project/blueprints/component/js/skins/component-skin.ts
+++ app/templates/project/blueprints/component/js/skins/component-skin.ts
@@ -1,17 +1,18 @@
 /// <reference path="../../../../../assets/vendor/terrific/dist/terrific.d.ts" />
 /// <reference path="../../../../../typings/tsd.d.ts" />
-/// <reference path="../<%= component.js%>.ts" />
+/// <reference path="../<%= component.js %>.ts" />
 
 module T {
     export module Module {
-        function <%= component.js %><%= skin.js %>(mod: <%= component.js%>) {
-            var start = mod.start.bind(mod);
-            mod.start = function (resolve: (value?: any) => void, reject: (error?: any) => void) {
-                console.log("start: <%= component.js %>.<%= skin.js %>");
-                start(resolve, reject);
-            };
+        export module <%= component.js %> {
+            export function <%= skin.js %>(mod: <%= component.js%>) {
+                var start = mod.start.bind(mod);
+                mod.start = function (resolve: (value?: any) => void, reject: (error?: any) => void) {
+                    console.log("start: <%= component.js %>.<%= skin.js %>");
+                    start(resolve, reject);
+                };
+            }
         }
-
-        Module['<%= component.js %>']['<%= skin.js %>'] = <%= component.js %><%= skin.js %>;
     }
 }
+

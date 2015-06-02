0bbb0568611e1346b35564a1f88ba88590120711
Christoph Buehler <christoph.buehler@namics.com>
2015-06-02 08:55:08 +0200

    Revert commit :( does not work with typescript compiler atm.

--- app/templates/components/organisms/Example/js/skins/example-blue.ts
+++ app/templates/components/organisms/Example/js/skins/example-blue.ts
@@ -4,26 +4,26 @@
 
 module T {
     export module Module {
-        export module Example {
-            export function Blue(mod:Example) {
-                var start = mod.start.bind(mod);
-                mod.start = function (resolve:(value?:any) => void, reject:(error?:any) => void) {
-                    var $ctx = $(mod._ctx);
+        function ExampleBlue(mod:Example) {
+            var start = mod.start.bind(mod);
+            mod.start = function (resolve:(value?:any) => void, reject:(error?:any) => void) {
+                var $ctx = $(this._ctx);
 
-                    console.log('Example Skin Blue - start id: [' + $ctx.data('t-id') + ']');
+                console.log('Example Skin Blue - start id: [' + $ctx.data('t-id') + ']');
 
-                    start(resolve, reject); // calling original method
-                };
+                start(resolve, reject); // calling original method
+            };
 
-                var sync = mod.sync.bind(mod);
-                mod.sync = function() {
-                    var $ctx = $(mod._ctx);
+            var sync = mod.sync.bind(mod);
+            mod.sync = function() {
+                var $ctx = $(this._ctx);
 
-                    console.log('Example Skin Blue - sync id: [' + $ctx.data('t-id') + ']');
+                console.log('Example Skin Blue - sync id: [' + $ctx.data('t-id') + ']');
 
-                    sync(); // calling original method
-                };
-            }
+                sync(); // calling original method
+            };
         }
+
+        Module['Example']['Blue'] = ExampleBlue;
     }
 }
--- app/templates/project/blueprints/component/js/skins/component-skin.ts
+++ app/templates/project/blueprints/component/js/skins/component-skin.ts
@@ -1,18 +1,17 @@
 /// <reference path="../../../../../assets/vendor/terrific/dist/terrific.d.ts" />
 /// <reference path="../../../../../typings/tsd.d.ts" />
-/// <reference path="../<%= component.js %>.ts" />
+/// <reference path="../<%= component.js%>.ts" />
 
 module T {
     export module Module {
-        export module <%= component.js %> {
-            export function <%= skin.js %>(mod: <%= component.js%>) {
-                var start = mod.start.bind(mod);
-                mod.start = function (resolve: (value?: any) => void, reject: (error?: any) => void) {
-                    console.log("start: <%= component.js %>.<%= skin.js %>");
-                    start(resolve, reject);
-                };
-            }
+        function <%= component.js %><%= skin.js %>(mod: <%= component.js%>) {
+            var start = mod.start.bind(mod);
+            mod.start = function (resolve: (value?: any) => void, reject: (error?: any) => void) {
+                console.log("start: <%= component.js %>.<%= skin.js %>");
+                start(resolve, reject);
+            };
         }
+
+        Module['<%= component.js %>']['<%= skin.js %>'] = <%= component.js %><%= skin.js %>;
     }
 }
-

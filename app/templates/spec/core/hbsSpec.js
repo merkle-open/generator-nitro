050503d89553a86daf9832e134552700983cf86f
ernscht <ernscht@gmail.com>
2016-01-09 20:40:04 +0100

    some small nitro test improvements

--- app/templates/spec/core/hbsSpec.js
+++ app/templates/spec/core/hbsSpec.js
@@ -3,4 +3,14 @@ describe('Nitro\'s Handlebars helper loader', function () {
 		var hbs = require('../../app/core/hbs');
 		expect(typeof hbs.handlebars.helpers.component).toBe('function');
 	});
+
+	it('has registered the view_list helper', function () {
+		var hbs = require('../../app/core/hbs');
+		expect(typeof hbs.handlebars.helpers.view_list).toBe('function');
+	});
+
+	it('has registered the t helper', function () {
+		var hbs = require('../../app/core/hbs');
+		expect(typeof hbs.handlebars.helpers.t).toBe('function');
+	});
 });

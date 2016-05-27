9c1db5b2373828bbb74a555433b51b8dd2ee2fe0
ernscht <ernscht@gmail.com>
2016-05-27 16:48:14 +0200

    add small test to ensure the right package name

---
+++ spec/generatorSpec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var pkg = require('../package.json');
+
+describe('Genearator', function () {
+
+	describe('Generator package configuration', function () {
+
+		it('contains the name `generator-nitro`', function () {
+			expect(pkg.name).toEqual('generator-nitro');
+		});
+
+		fit('includes MIT license', function () {
+			expect(pkg.license).toEqual('MIT');
+		});
+
+	});
+
+});
+

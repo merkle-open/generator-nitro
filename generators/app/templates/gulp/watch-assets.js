0ee522c21d15af0d9aef524e240b7c251de5487f
ernscht <ernscht@gmail.com>
2017-02-21 23:19:09 +0100

    improve asset compiling for multiple sources

--- generators/app/templates/gulp/watch-assets.js
+++ generators/app/templates/gulp/watch-assets.js
@@ -5,61 +5,88 @@ const utils = require('./utils');
 const globby = require('globby');
 
 module.exports = (gulp, plugins) => {
+
+	function isDependentStyleSource(file) {
+		let isDependent = false;
+		utils.getSourcePatterns('css').forEach((asset) => {
+			globby.sync(asset.deps).forEach((path) => {
+				if ( file.replace(/\\/g, '/').endsWith(path) ) {
+					isDependent = true;
+				}
+			});
+		});
+		return isDependent;
+	}
+	function clearJsCache() {
+		utils.getSourcePatterns('js').forEach((asset) => {
+			if (plugins.cached.caches && plugins.cached.caches[asset.name]) {
+				delete plugins.cached.caches[asset.name];
+			}
+			if (plugins.remember.cacheFor(asset.name)) {
+				plugins.remember.forgetAll(asset.name);
+			}
+		});
+	}
+	function clearCssCache() {
+		utils.getSourcePatterns('css').forEach((asset) => {
+			if (plugins.cached.caches && plugins.cached.caches[asset.name]) {
+				delete plugins.cached.caches[asset.name];
+			}
+			if (plugins.remember.cacheFor(asset.name)) {
+				plugins.remember.forgetAll(asset.name);
+			}
+		});
+	}
+	function checkCssCache(e) {
+		if (
+			isDependentStyleSource(e.path) ||
+			'unlink' === e.event
+		) {
+			clearCssCache();
+		}
+	}
+	function clearCache() {
+		clearJsCache();
+		clearCssCache();
+	}
+
+	const lastRun = {};
+	function processChange(type, func, throttle) {
+		type = type || 'other';
+		func = func || function(){};
+		throttle = throttle || 1000;
+
+		// call function only once in defined time
+		lastRun[type] = lastRun[type] || 0;
+		if (new Date() - lastRun[type] > throttle) {
+			func();
+		}
+		lastRun[type] = new Date();
+	}
+
 	return () => {
 		const browserSync = utils.getBrowserSyncInstance();
-		const isDependentStyleSource = (file) => {
-			let isDependent = false;
-			const cssAssets = utils.getSourcePatterns('css');
-			cssAssets.forEach((asset) => {
-				globby.sync(asset.deps).forEach((path) => {
-					if ( file.replace(/\\/g, '/').endsWith(path) ) {
-						isDependent = true;
-					}
-				});
-			});
-
-			return isDependent;
-		};
-		const clearCache = (e) => {
-			if (
-				'unlink' === e.event ||
-				'add' === e.event ||
-				e.path.endsWith('config.json') ||
-				isDependentStyleSource(e.path)
-			) {
-				// forget all
-				plugins.cached.caches = {};
-				const cssAssets = utils.getSourcePatterns('css');
-				cssAssets.forEach((asset) => {
-					if (plugins.remember.cacheFor(asset.name)) {
-						plugins.remember.forgetAll(asset.name);
-					}
-				});
-				const jsAssets = utils.getSourcePatterns('js');
-				jsAssets.forEach((asset) => {
-					if (plugins.remember.cacheFor(asset.name)) {
-						plugins.remember.forgetAll(asset.name);
-					}
-				});
-			}
-		};
 
 		plugins.watch([
 			'config.json'
-		], (e) => {
-			config = utils.reloadConfig();
-			clearCache(e);
-			utils.updateSourcePatterns();
-			gulp.start('compile-css');
-			gulp.start('compile-js');
+		], () => {
+			processChange('config', function() {
+				config = utils.reloadConfig();
+				clearCache();
+				utils.updateSourcePatterns();
+				gulp.start('compile-css');
+				gulp.start('compile-js');
+			}, 2000);
 		});
 
 		plugins.watch([
 			'assets/css/**/*.<%= options.pre %>',
 			'patterns/**/css/**/*.<%= options.pre %>'
 		], (e) => {
-			clearCache(e);
-			gulp.start('compile-css');
+			processChange('css', function() {
+				checkCssCache(e);
+				gulp.start('compile-css');
+			});
 		});
 
 		plugins.watch([
@@ -69,7 +96,9 @@ module.exports = (gulp, plugins) => {
 			'patterns/**/js/**/*.ts'<% } %><% if (options.clientTpl) { %>,
 			'patterns/**/template/**/*.hbs'<% } %>
 		], () => {
-			gulp.start('compile-js');
+			processChange('js', function() {
+				gulp.start('compile-js');
+			});
 		});
 
 		plugins.watch([
@@ -80,7 +109,9 @@ module.exports = (gulp, plugins) => {
 			'patterns/**/schema.json',
 			'patterns/**/_data/*.json'
 		], () => {
-			browserSync.reload();
+			processChange('data', function() {
+				browserSync.reload();
+			});
 		});
 
 		plugins.watch([

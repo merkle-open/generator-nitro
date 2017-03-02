e8ba81eaf9412da64856965e0dd8174527a9cf44
Christian Stuff <christian.stuff@namics.com>
2017-03-02 16:15:01 +0100

    Only start server if views should be exported

--- packages/nitro-exporter/index.js
+++ packages/nitro-exporter/index.js
@@ -4,15 +4,18 @@ const exportViews = require('./tasks/export-views.js');
 const exportProcessing = require('./tasks/export-processing.js');
 
 module.exports = function (gulp, config) {
+	const shouldExportViews = !config.exporter.length ? config.exporter.views :
+		config.exporter.some((exporterConfig) => exporterConfig.views === true);
+
 	gulp.task('export-clean', exportClean(config));
 	gulp.task('export-views', () => exportViews(gulp, config));
 	gulp.task('export-processing', () => exportProcessing(gulp, config));
 	gulp.task('export', gulpSequence(
 		'export-clean',
 		'assets',
-		'serve',
-		'export-views',
-		'export-processing',
-		'serve-stop'
+		shouldExportViews ? 'serve' : '',
+		shouldExportViews ? 'export-views' : '',
+		shouldExportViews ? 'serve-stop' : '',
+		'export-processing'
 	));
 };

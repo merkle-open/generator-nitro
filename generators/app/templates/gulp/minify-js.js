53642aee6dbecb83a8801558204984217b7ac2f6
ernscht <ernscht@gmail.com>
2017-05-18 23:21:11 +0200

    nitro - preserve some comments in minified js files

--- generators/app/templates/gulp/minify-js.js
+++ generators/app/templates/gulp/minify-js.js
@@ -9,7 +9,7 @@ module.exports = (gulp, plugins) => {
 		assets.forEach((asset) => {
 			gulp
 				.src(`public/assets/js/${asset.name}`)
-				.pipe(plugins.uglify())
+				.pipe(plugins.uglify({preserveComments: 'license'}))
 				.pipe(plugins.rename(asset.name.replace('.js', '.min.js')))
 				.pipe(plugins.size({showFiles: true, gzip: false, title: 'JavaScript minified'}))
 				.pipe(plugins.size({showFiles: true, gzip: true, title: 'JavaScript minified'}))

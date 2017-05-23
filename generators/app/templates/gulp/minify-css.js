7463ecd683338c67808cd3191c0b0615b1481df2
ernscht <ernscht@gmail.com>
2017-05-23 22:08:37 +0200

    nitro - keep gulp chain also for minification

--- generators/app/templates/gulp/minify-css.js
+++ generators/app/templates/gulp/minify-css.js
@@ -1,24 +1,31 @@
 'use strict';
 
 const utils = require('./utils');
+const Promise = require('es6-promise').Promise;
 
 module.exports = (gulp, plugins) => {
 	return () => {
 		const assets = utils.getSourcePatterns('css');
+		let promises = [];
 
 		assets.forEach((asset) => {
-			gulp
-				.src(`public/assets/css/${asset.name}`)
-				.pipe(plugins.cssnano({
-					mergeRules: false,
-					safe: true
-				}))
-				.pipe(plugins.rename(asset.name.replace('.css', '.min.css')))
-				.pipe(plugins.size({showFiles: true, gzip: false, title: 'CSS minified'}))
-				.pipe(plugins.size({showFiles: true, gzip: true, title: 'CSS minified'}))
-				.pipe(gulp.dest('public/assets/css/'));
+			promises.push(new Promise((resolve) => {
+				gulp
+					.src(`public/assets/css/${asset.name}`)
+					.pipe(plugins.cssnano({
+						mergeRules: false,
+						safe: true
+					}))
+					.pipe(plugins.rename(asset.name.replace('.css', '.min.css')))
+					.pipe(plugins.size({showFiles: true, gzip: false, title: 'CSS minified'}))
+					.pipe(plugins.size({showFiles: true, gzip: true, title: 'CSS minified'}))
+					.pipe(gulp.dest('public/assets/css/'))
+					.on('end', () => {
+						resolve();
+					});
+			}));
 		});
 
-		return gulp;
+		return Promise.all(promises);
 	};
 };
--- generators/app/templates/gulp/minify-js.js
+++ generators/app/templates/gulp/minify-js.js
@@ -1,28 +1,33 @@
 'use strict';
 
 const utils = require('./utils');
+const Promise = require('es6-promise').Promise;
 const uglifyOptions = {
 	output: {
 		comments: /^!/
 	}
 };
 
-
 module.exports = (gulp, plugins) => {
 	return () => {
 		const assets = utils.getSourcePatterns('js');
+		let promises = [];
 
 		assets.forEach((asset) => {
-			gulp
-				.src(`public/assets/js/${asset.name}`)
-				.pipe(plugins.uglify(uglifyOptions))
-				.pipe(plugins.rename(asset.name.replace('.js', '.min.js')))
-				.pipe(plugins.size({showFiles: true, gzip: false, title: 'JavaScript minified'}))
-				.pipe(plugins.size({showFiles: true, gzip: true, title: 'JavaScript minified'}))
-				.pipe(gulp.dest('public/assets/js/'));
+			promises.push(new Promise((resolve) => {
+				gulp
+					.src(`public/assets/js/${asset.name}`)
+					.pipe(plugins.uglify(uglifyOptions))
+					.pipe(plugins.rename(asset.name.replace('.js', '.min.js')))
+					.pipe(plugins.size({showFiles: true, gzip: false, title: 'JavaScript minified'}))
+					.pipe(plugins.size({showFiles: true, gzip: true, title: 'JavaScript minified'}))
+					.pipe(gulp.dest('public/assets/js/'))
+					.on('end', () => {
+						resolve();
+					});
+			}));
 		});
 
-		return gulp;
+		return Promise.all(promises);
 	};
 };
-

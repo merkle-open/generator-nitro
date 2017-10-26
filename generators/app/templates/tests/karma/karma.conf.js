16a39ece3b62099887a98aee5132bd9ad5ae94e2
ernscht <ernscht@gmail.com>
2017-10-26 15:05:33 +0200

    #119 rename pattern test files & directories - use test instead of spec

--- generators/app/templates/tests/karma/karma.conf.js
+++ generators/app/templates/tests/karma/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
 		// list of files / patterns to load in the browser
 		files: [
 			'public/assets/js/app.js',
-			'src/patterns/**/spec/*Spec.js',
+			'src/patterns/**/test/*.test.js',
 		],
 
 		// list of files to exclude
@@ -27,11 +27,11 @@ module.exports = function (config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'src/patterns/**/spec/*Spec.js': ['babel'],
+			'src/patterns/**/test/*.test.js': ['babel'],
 		},
 		babelPreprocessor: {
 			options: {
-				presets: ['es2015'],
+				presets: ['env'],
 				sourceMap: 'inline',
 			},
 		},

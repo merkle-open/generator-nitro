9c46c505e96685cc3a4e2b621b7bc3b8f4d8bd6f
ernscht <ernscht@gmail.com>
2018-01-16 11:17:48 +0100

    update eslint config and .gitignore

--- packages/nitro-exporter/.eslintrc.js
+++ packages/nitro-exporter/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-	"extends": "@namics/eslint-config/configurations/es6-node.js",
-	"rules": {
-		'complexity': [2, 8],
-		"consistent-return": 0,
-		"no-console": 0
-	}
+	'extends': '@namics/eslint-config/configurations/es6-node.js',
+	'rules': {
+		'complexity': ['error', 10],
+		'consistent-return': 0,
+		'no-console': 0,
+	},
 };

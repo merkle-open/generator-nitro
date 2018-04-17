c4f07c7f8603c1b4bfc9f784c56a6ddbe6d9b459
ernscht <ernscht@gmail.com>
2018-04-17 10:24:36 +0200

    nitro: move i18next configuration to config

--- generators/app/templates/app/core/i18n.js
+++ generators/app/templates/app/core/i18n.js
@@ -4,11 +4,7 @@
  * Init Translation Library i18next
  * Documentation: https://www.npmjs.com/package/i18next-express-middleware
  *
- * Configuration:
- * Fallback translation file: project/locales/default/translation.json
- * Other languages in project/locales/[lang]/translation.json
- * Language detection from request header
- * Language switch with query parameter: ?lang=de
+ * Configuration in config package ('./config.js').
  *
  * Use with handlebars translation helper t: `../templating/hbs/helpers/t.js`
  */
@@ -16,25 +12,11 @@ const i18next = require('i18next');
 const FilesystemBackend = require('i18next-node-fs-backend');
 const sprintf = require('i18next-sprintf-postprocessor');
 const i18nextMiddleware = require('i18next-express-middleware');
+const config = require('config');
 
-const options = {
-	// defaultNS: 'translation',
-	// whitelist: ['en', 'de', 'default'],
-	fallbackLng: 'default',
-	backend: {
-		'loadPath': 'project/locales/{{lng}}/{{ns}}.json',
-	},
-	detection: {
-		// order and from where user language should be detected
-		order: ['querystring', 'header'],
-		// keys or params to lookup language from
-		lookupQuerystring: 'lang',
-	},
-	debug: false,
-};
-const middlewareOptions = {
-	ignoreRoutes: ['api/', 'assets/', 'dist/', 'proto/'],
-};
+// The middleware changes options, so we have to clone the locked objects
+const options = Object.assign({}, config.get('feature.i18next.options'));
+const middlewareOptions = Object.assign({}, config.get('feature.i18next.middlewareOptions'));
 
 i18next
 	.use(i18nextMiddleware.LanguageDetector)

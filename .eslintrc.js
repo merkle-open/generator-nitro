195d46e4f5f17232259f248e19407edc428212ea
ernscht <ernscht@gmail.com>
2018-05-25 18:09:02 +0200

    chore: add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	extends: [
+		'@namics/eslint-config/configurations/es8-node.js',
+		'@namics/eslint-config/configurations/es8-node-disable-styles.js',
+	].map(require.resolve),
+	globals: {
+		__dirname: true,
+	},
+	'rules': {
+		'require-jsdoc': 'off',
+	},
+};

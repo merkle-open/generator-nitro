e3187052e34969f1598265643a05f12e3a673c7e
ernscht <ernscht@gmail.com>
2018-06-29 15:55:01 +0200

    chore(exporter): make exporter lerna compatible

--- packages/nitro-exporter/.eslintrc.js
+++ packages/nitro-exporter/.eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-	'extends': '@namics/eslint-config/configurations/es6-node.js',
+	extends: [
+		'@namics/eslint-config/configurations/es8-node.js',
+		'@namics/eslint-config/configurations/es8-node-disable-styles.js',
+	].map(require.resolve),
 	'rules': {
-		'complexity': ['error', 10],
-		'consistent-return': 0,
-		'no-console': 0,
+		'complexity': 'off',
+		'global-require': 'off',
+		'new-cap': 'off',
+		'no-console': 'off',
+		'no-bitwise': 'off',
+		'no-inline-comments': 'off',
+		'no-implicit-coercion': 'off',
+		'prefer-rest-params': 'off',
+		'require-jsdoc': 'off',
+		'valid-jsdoc': 'off'
 	},
 };
--- packages/nitro-exporter/readme.md
+++ packages/nitro-exporter/readme.md
@@ -4,7 +4,7 @@ With this package, Nitro can generate static exports of your project by using `n
 
     $ npm run export
 
-The exporter configuration can be found in [config.json](//github.com/namics/generator-nitro/app/templates/config.json).
+The exporter configuration can be found in [config](//github.com/namics/generator-nitro/app/templates/config.json).
 
 ## Configuration options
 

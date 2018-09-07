4f0678b2c71ddd49be17ee853ea6c572311750ac
ernscht <ernscht@gmail.com>
2018-09-07 13:43:30 +0200

    fix(project): improve update script

--- packages/generator-nitro/generators/app/templates/stylelint.config.js
+++ packages/generator-nitro/generators/app/templates/stylelint.config.js
@@ -2,7 +2,7 @@ const lintConfig = require('@namics/stylelint-config');
 
 lintConfig.rules['plugin/stylelint-bem-namics'] = {
 	patternPrefixes: ['a', 'm', 'o', 'h'],
-	helperPrefixes: ['state']
+	helperPrefixes: ['state'],
 };
 
 module.exports = lintConfig;

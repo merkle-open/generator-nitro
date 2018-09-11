29b376b7a098692bca06ecae055fc87b60cc6d94
ernscht <ernscht@gmail.com>
2018-09-11 11:26:48 +0200

    test(project): use puppeteer as default engine for visual testing

--- packages/project-nitro-twig/tests/backstop/backstop.config.js
+++ packages/project-nitro-twig/tests/backstop/backstop.config.js
@@ -30,15 +30,20 @@ module.exports = (options) => {
 	const scenarios = [
 		{
 			label: 'Homepage',
+			// cookiePath: 'tests/backstop/engine_scripts/cookies.json',
 			url: `http://localhost:${options.port}/index`,
-			onBeforeScript: 'onBefore.js',
-			readyEvent: null,
-			delay: 1000,
+			referenceUrl: '',
+			readyEvent: '',
+			readySelector: '',
+			delay: 0,
 			hideSelectors: [],
 			removeSelectors: ['.m-example--blue'],
-			onReadyScript: 'onReady.js',
+			hoverSelector: '',
+			clickSelector: '',
+			postInteractionWait: 0,
 			selectors: ['.m-example'],
 			selectorExpansion: true,
+			expect: 2,
 			misMatchThreshold: 0.1,
 			requireSameDimensions: true,
 		},
@@ -47,6 +52,8 @@ module.exports = (options) => {
 	return {
 		id: 'nitro_test',
 		viewports,
+		onBeforeScript: 'puppet/onBefore.js',
+		onReadyScript: 'puppet/onReady.js',
 		scenarios,
 		paths: {
 			bitmaps_reference: `tests/backstop/bitmaps_reference/${platform}`,
@@ -55,13 +62,17 @@ module.exports = (options) => {
 			html_report: 'public/reports/backstop/html',
 			ci_report: 'project/tmp/reports/backstop/ci',
 		},
-		engineFlags: [],
-		engine: 'chrome',
 		report: ['browser'],
+		engineFlags: [],
+		engine: 'puppeteer',
+		engineOptions: {
+			args: ['--no-sandbox'],
+		},
 		resembleOutputOptions: {
 			ignoreAntialiasing: true,
 		},
 		asyncCaptureLimit: 5,
+		asyncCompareLimit: 50,
 		debug: false,
 		debugWindow: false,
 	};

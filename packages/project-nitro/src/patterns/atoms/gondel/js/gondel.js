72e624911b5b685a1c6cc82017bf8d7516fa4f38
ernscht <ernscht@gmail.com>
2018-10-09 22:21:41 +0200

    refactor(project): improve gondel example

--- packages/project-nitro/src/patterns/atoms/gondel/js/gondel.js
+++ packages/project-nitro/src/patterns/atoms/gondel/js/gondel.js
@@ -3,11 +3,20 @@
 import { Component, EventListener, GondelBaseComponent } from '@gondel/core';
 
 const states = {
+	prepare: 'state-a-gondel--prepare',
 	motion: 'state-a-gondel--motion',
 };
 
 @Component('Gondel')
 class Gondel extends GondelBaseComponent {
+	@EventListener('mouseover')
+	_handleMouseOver() {
+		this._ctx.classList.add(states.prepare);
+	}
+	@EventListener('mouseout')
+	_handleMouseOut() {
+		this._ctx.classList.remove(states.prepare);
+	}
 	@EventListener('click')
 	_handleChange() {
 		this._ctx.classList.toggle(states.motion);

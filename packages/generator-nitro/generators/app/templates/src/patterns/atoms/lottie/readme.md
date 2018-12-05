11775a00671984f916ebde9510575735f70dd753
ernscht <ernscht@gmail.com>
2018-12-05 09:12:34 +0100

    feature(project): add lottie example

---
+++ packages/generator-nitro/generators/app/templates/src/patterns/atoms/lottie/readme.md
@@ -0,0 +1,12 @@
+# lottie
+
+* Displays a [lottie](https://github.com/airbnb/lottie-web/) animation.
+* Requires request on a json file
+
+## Testing
+
+[Example page](http://localhost:8081/example-patterns)
+
+## Issues
+
+Not every animation will work on all browsers
---
+++ packages/project-nitro/project/routes/lottie.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Usage:
+ *  /api/lottie/:animation
+ *
+ * Returns lottie animation json
+ */
+
+const path = require('path');
+const fs = require('fs');
+const utils = require('./helpers/utils.js');
+
+function animation(req, res, next) {
+	// load animation data
+	const file = path.join(__dirname, './data/lottie',  req.params.animation);
+	let data = {'error':true};
+	if (fs.existsSync(file)) {
+		data = JSON.parse(fs.readFileSync(file));
+	}
+
+	setTimeout(() => {
+		return res.json(data);
+	}, utils.getRandomInt(250, 1000));
+}
+
+module.exports = (app) => {
+	app.route('/api/lottie/:animation').get(animation);
+};

77dff08d2230f67f1b047b4314d26cccc4a59077
ernscht <ernscht@gmail.com>
2018-12-09 12:32:43 +0100

    chore: update config to version 3

--- packages/nitro-app/app/core/i18n.js
+++ packages/nitro-app/app/core/i18n.js
@@ -13,11 +13,12 @@ const FilesystemBackend = require('i18next-node-fs-backend');
 const sprintf = require('i18next-sprintf-postprocessor');
 const i18nextMiddleware = require('i18next-express-middleware');
 const config = require('config');
+const cloneDeep = require('lodash/cloneDeep');
 
-// The middleware changes options, so we have to clone the locked objects
+// The middleware changes options, so we have to deep clone the locked objects
 const usei18next = !!config.has('feature.i18next') && config.get('feature.i18next');
-const options = config.has('feature.i18next.options') ? Object.assign({}, config.get('feature.i18next.options')) : {};
-const middlewareOptions = config.has('feature.i18next.middlewareOptions') ? Object.assign({}, config.get('feature.i18next.middlewareOptions')) : {};
+const options = config.has('feature.i18next.options') ? cloneDeep(config.get('feature.i18next.options')) : {};
+const middlewareOptions = config.has('feature.i18next.middlewareOptions') ? cloneDeep(config.get('feature.i18next.middlewareOptions')) : {};
 
 if (usei18next) {
 	i18next

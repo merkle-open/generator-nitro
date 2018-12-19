7b8e99e139974e6cdf4a989a9d537e9cf9bcb5a5
ernscht <ernscht@gmail.com>
2018-12-19 09:27:50 +0100

    docs(exporter): improve exporter example

--- packages/generator-nitro/generators/app/templates/config/default/exporter.js
+++ packages/generator-nitro/generators/app/templates/config/default/exporter.js
@@ -6,65 +6,67 @@
  */
 
 const config = {
-	exporter: {
-		dest: 'dist',
-		i18n: [],
-		publics: ['public/*', 'public/assets/**/*', 'public/content/**/*'],
-		renames: [
-			{
-				src: 'dist/assets/**',
-				base: 'dist/assets',
-				dest: 'dist/',
-			},
-		],
-		replacements: [
-			{
-				glob: ['dist/*.html'],
-				replace: [
-					{
-						from: '/assets/',
-						to: '',
-					},
-					{
-						from: '/content/',
-						to: 'content/',
-					},
-					{
-						from: ' href="([a-z\-]+)"',
-						to: ' href="$1.html"',
-					},
-				],
-			},
-			{
-				glob: ['dist/css/*.css'],
-				replace: [
-					{
-						from: '/assets/',
-						to: '../',
-					},
-					{
-						from: '/content/',
-						to: '../content/',
-					},
-				],
-			},
-			{
-				glob: ['dist/js/*.js'],
-				replace: [
-					{
-						from: '/assets/',
-						to: '',
-					},
-					{
-						from: '/api',
-						to: 'api',
-					},
-				],
-			},
-		],
-		views: true,
-		zip: false,
-	},
+	exporter: [
+		{
+			dest: 'dist',
+			i18n: [],
+			publics: ['public/*', 'public/assets/**/*', 'public/content/**/*'],
+			renames: [
+				{
+					src: 'dist/assets/**',
+					base: 'dist/assets',
+					dest: 'dist/',
+				},
+			],
+			replacements: [
+				{
+					glob: ['dist/*.html'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '',
+						},
+						{
+							from: '/content/',
+							to: 'content/',
+						},
+						{
+							from: ' href="\/?([a-z0-9\-]+)"',
+							to: ' href="$1.html"',
+						},
+					],
+				},
+				{
+					glob: ['dist/css/*.css'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '../',
+						},
+						{
+							from: '/content/',
+							to: '../content/',
+						},
+					],
+				},
+				{
+					glob: ['dist/js/*.js'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '',
+						},
+						{
+							from: '/api',
+							to: 'api',
+						},
+					],
+				},
+			],
+			views: true,
+			zip: false,
+		},
+	],
 };
 
 module.exports = config.exporter;
--- packages/project-nitro/config/default/exporter.js
+++ packages/project-nitro/config/default/exporter.js
@@ -6,65 +6,124 @@
  */
 
 const config = {
-	exporter: {
-		dest: 'dist',
-		i18n: [],
-		publics: ['public/*', 'public/assets/**/*', 'public/content/**/*'],
-		renames: [
-			{
-				src: 'dist/assets/**',
-				base: 'dist/assets',
-				dest: 'dist/',
-			},
-		],
-		replacements: [
-			{
-				glob: ['dist/*.html'],
-				replace: [
-					{
-						from: '/assets/',
-						to: '',
-					},
-					{
-						from: '/content/',
-						to: 'content/',
-					},
-					{
-						from: ' href="([a-z\-]+)"',
-						to: ' href="$1.html"',
-					},
-				],
-			},
-			{
-				glob: ['dist/css/*.css'],
-				replace: [
-					{
-						from: '/assets/',
-						to: '../',
-					},
-					{
-						from: '/content/',
-						to: '../content/',
-					},
-				],
-			},
-			{
-				glob: ['dist/js/*.js'],
-				replace: [
-					{
-						from: '/assets/',
-						to: '',
-					},
-					{
-						from: '/api',
-						to: 'api',
-					},
-				],
-			},
-		],
-		views: true,
-		zip: false,
-	},
+	exporter: [
+		{
+			dest: 'dist',
+			i18n: [],
+			publics: ['public/*', 'public/assets/**/*', 'public/content/**/*'],
+			renames: [
+				{
+					src: 'dist/assets/**',
+					base: 'dist/assets',
+					dest: 'dist/',
+				},
+			],
+			replacements: [
+				{
+					glob: ['dist/*.html'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '',
+						},
+						{
+							from: '/content/',
+							to: 'content/',
+						},
+						{
+							from: ' href="\/?([a-z0-9\-]+)"',
+							to: ' href="$1.html"',
+						},
+					],
+				},
+				{
+					glob: ['dist/css/*.css'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '../',
+						},
+						{
+							from: '/content/',
+							to: '../content/',
+						},
+					],
+				},
+				{
+					glob: ['dist/js/*.js'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '',
+						},
+						{
+							from: '/api',
+							to: 'api',
+						},
+					],
+				},
+			],
+			views: ['index', 'example-*'],
+			zip: false,
+		},
+		{
+			dest: 'target',
+			i18n: [],
+			publics: ['public/*', 'public/assets/**/*', 'public/content/**/*'],
+			renames: [],
+			replacements: [
+				{
+					glob: ['target/*.html'],
+					replace: [
+						{
+							from: '/assets/',
+							to: 'assets/',
+						},
+						{
+							from: '/content/',
+							to: 'content/',
+						},
+						{
+							from: 'example-patterns',
+							to: '404',
+						},
+						{
+							from: ' href="\/?([a-z0-9\-]+)"',
+							to: ' href="$1.html"',
+						},
+					],
+				},
+				{
+					glob: ['target/assets/css/*.css'],
+					replace: [
+						{
+							from: '/assets/',
+							to: '../',
+						},
+						{
+							from: '/content/',
+							to: '../content/',
+						},
+					],
+				},
+				{
+					glob: ['target/assets/js/*.js'],
+					replace: [
+						{
+							from: '/assets/',
+							to: 'assets/',
+						},
+						{
+							from: '/api',
+							to: 'api',
+						},
+					],
+				},
+			],
+			views: ['index', '404'],
+			zip: true,
+		},
+	],
 };
 
 module.exports = config.exporter;

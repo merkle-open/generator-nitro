f03192fc350f29a54b028f2d91ba03ac4cdcceca
ernscht <ernscht@gmail.com>
2019-01-09 11:51:12 +0100

    chore(project): add @namics/prettier-config

--- packages/generator-nitro/generators/app/templates/.prettierrc.js
+++ packages/generator-nitro/generators/app/templates/.prettierrc.js
@@ -1,26 +1,3 @@
 module.exports = {
-	printWidth: 120,
-	tabWidth: 4,
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	trailingComma: 'es5',
-	bracketSpacing: true,
-	jsxBracketSameLine: false,
-	arrowParens: 'always',
-	parser: 'typescript',
-	overrides: [
-		{
-			files: ['*.json', '.*.json'],
-			options: {
-				parser: 'json',
-			},
-		},
-		{
-			files: ['*.(js|jsx)', '.*.(js|jsx)'],
-			options: {
-				parser: 'babylon',
-			},
-		},
-	],
+	...require('@namics/prettier-config'),
 };

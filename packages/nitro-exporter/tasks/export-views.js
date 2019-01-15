34a767cd417749b760b97af5fe111472485e8f77
ernscht <ernscht@gmail.com>
2019-01-15 09:16:39 +0100

    feature(exporter): allow additional routes

--- packages/nitro-exporter/tasks/export-views.js
+++ packages/nitro-exporter/tasks/export-views.js
@@ -11,29 +11,34 @@ module.exports = function (gulp, config) {
 	utils.each(config.exporter, (configEntry) => {
 		processes.push(
 			new Promise((resolve) => {
-				const ext = '(html|json)';
-				const i18nFilter = configEntry.i18n.length ?
-					configEntry.i18n.map((lng) => path.join(nitroTmpDirectory, `*-${lng}.${ext}`)) :
-					path.join(nitroTmpDirectory, '**', `*.${ext}`);
+				// allowed extensions for views and additional views
+				const extPattern = '{html,json}';
+				// check for language configuration
+				const langPattern = configEntry.i18n.length
+					? `-${configEntry.i18n.filter((lang) => lang !== 'default').join(',')}`
+					: '';
 
-				if (configEntry.views === true) {
-					gulp.src(path.join(nitroTmpDirectory, '**', `*.${ext}`))
-						.pipe(filter(i18nFilter))
-						.pipe(gulp.dest(configEntry.dest))
-						.on('end', () => {
-							resolve();
-						});
-				} else if (Array.isArray(configEntry.views) && configEntry.views.length > 0) {
-					gulp.src(path.join(nitroTmpDirectory, '**', `*.${ext}`))
-						.pipe(filter(configEntry.views.map((v) => path.join(nitroTmpDirectory, '**', `${v}.${ext}`))))
-						.pipe(filter(i18nFilter))
-						.pipe(gulp.dest(configEntry.dest))
-						.on('end', () => {
-							resolve();
-						});
-				} else {
-					resolve();
-				}
+				const hasViewArray = Array.isArray(configEntry.views) && configEntry.views.length > 0;
+				// views does not have file extensions
+				const viewPattern = hasViewArray
+					? `{${configEntry.views.filter((view) => !path.extname(view)).join(',')}}`
+					: '*';
+				// additional views must have a file extension
+				const additionalViews = hasViewArray
+					? configEntry.views.filter((view) => path.extname(view)).map((view) => path.join(nitroTmpDirectory, view))
+					: [];
+				// merge dumped views with additional views
+				const viewFilter = [
+					path.join(nitroTmpDirectory, `${viewPattern}${langPattern}.html`),
+					...additionalViews,
+				];
+
+				gulp.src(path.join(nitroTmpDirectory, '**', `*.${extPattern}`))
+					.pipe(filter(viewFilter))
+					.pipe(gulp.dest(configEntry.dest))
+					.on('end', () => {
+						resolve();
+					});
 			})
 		);
 	});

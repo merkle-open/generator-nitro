c6d69b61723de93a2ab63eb7ac38b651927f793a
ernscht <ernscht@gmail.com>
2019-01-15 09:16:39 +0100

    feature(exporter): allow additional routes

--- packages/nitro-exporter/index.js
+++ packages/nitro-exporter/index.js
@@ -1,25 +1,44 @@
 'use strict';
 
+/* global process */
+
 const exportClean = require('./tasks/export-clean.js');
 const exportViews = require('./tasks/export-views.js');
 const exportProcessing = require('./tasks/export-processing.js');
 
-module.exports = function (gulp, config) {
-	const gulpSequence = require('gulp-sequence').use(gulp);
-	const shouldExportViews = !config.exporter.length ?
+function getMergedConfigProps(config) {
+	const collectedConfig = {};
+
+	collectedConfig.shouldExportViews = !config.exporter.length ?
 		config.exporter.views :
 		config.exporter.some((exporterConfig) => exporterConfig.views !== false);
+
+	collectedConfig.languages = !config.exporter.length ?
+		config.exporter.i18n :
+		config.exporter.reduce((acc, exporterConfig) => [...acc, ...exporterConfig.i18n], ['default']);
+
+	return collectedConfig;
+}
+
+module.exports = function (gulp, config) {
+	const gulpSequence = require('gulp-sequence').use(gulp);
+	const mergedConfig = getMergedConfigProps(config);
 
 	gulp.task('export-clean', exportClean(config));
 	gulp.task('export-views', () => exportViews(gulp, config));
 	gulp.task('export-processing', () => exportProcessing(gulp, config));
 
-	if (shouldExportViews) {
+	if (mergedConfig.shouldExportViews) {
+		if (mergedConfig.languages.length) {
+			// set global env to use in other gulp tasks
+			process.env.NITRO_VIEW_LOCALES = mergedConfig.languages.join(',');
+		}
+
 		gulp.task('export', gulpSequence(
 			'export-clean',
 			'dump-views',
 			'export-views',
-			'export-processing'
+			'export-processing',
 		));
 	} else {
 		gulp.task('export', gulpSequence(

ac5f0d33fe2c22ca7c81fbf5ca1189627acf2c4c
Jan Widmer <jan.widmer@namics.com>
2019-05-06 16:20:02 +0200

    feature(app): add filter option to the viewlist helper for hbs

--- packages/nitro-app/app/templating/hbs/helpers/viewlist.js
+++ packages/nitro-app/app/templating/hbs/helpers/viewlist.js
@@ -1,16 +1,43 @@
 'use strict';
 
+const path = require('path');
 const hbs = require('hbs');
 const config = require('config');
 const view = require('../../../lib/view.js');
 
 module.exports = function () {
-	const views = view.getViews(config.get('nitro.basePath') + config.get('nitro.viewDirectory'));
+	const context = arguments[arguments.length - 1];
+	const viewIncludes = context.hash.viewIncludes ? context.hash.viewIncludes : '';
+	const viewExcludes = context.hash.viewExcludes ? context.hash.viewExcludes : '';
+
+	const views = view.getViews(path.join(config.get('nitro.basePath'), config.get('nitro.viewDirectory')));
 	const markup = ['<ul>'];
 
-	views.forEach((viewItem) => {
+	let filteredViews = views;
+
+	if (viewIncludes !== '') {
+		const viewIncludesArray = viewIncludes.split(';');
+		filteredViews = views.filter((viewItem) => {
+			return viewIncludesArray.some((includeString) => {
+				return viewItem.url.indexOf(includeString) >= 0;
+			});
+		});
+	} else if (viewExcludes !== '') {
+		const viewExcludesArray = viewExcludes.split(';');
+		filteredViews = views.filter((viewItem) => {
+			return viewExcludesArray.every((excludeString) => {
+				return viewItem.url.indexOf(excludeString) === -1;
+			});
+		});
+	}
+
+	filteredViews.forEach((viewItem) => {
 		markup.push(`<li><a href="/${viewItem.url}">${viewItem.name}</a></li>`);
 	});
+
+	if (filteredViews.length === 0) {
+		markup.push('<li>For this include / exclude term, no pages where found</li>');
+	}
 
 	markup.push('</ul>');
 

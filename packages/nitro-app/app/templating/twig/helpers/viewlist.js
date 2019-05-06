d6165033b2ab1eaf60febcd0c393d15a385e35a3
Jan Widmer <jan.widmer@namics.com>
2019-05-06 16:30:27 +0200

    feature(app): add filter option to the viewlist helper for twig

--- packages/nitro-app/app/templating/twig/helpers/viewlist.js
+++ packages/nitro-app/app/templating/twig/helpers/viewlist.js
@@ -15,23 +15,54 @@ const twigUtils = require('../utils');
 module.exports = function () {
 	return {
 		type: 'viewlist',
-		regex: /^viewlist$/,
+		regex: /^viewlist\s*([\S\s]+?)?$/,
 		next: [],
 		open: true,
 		compile (token) {
+			const data = (typeof token.match[1] !== 'undefined') ? token.match[1] : '';
+
+			token.variantStack = Twig.expression.compile.apply(this, [{
+				type: Twig.expression.type.expression,
+				value: data.trim()
+			}]).stack;
+
 			delete token.match;
 			return token;
 		},
 		parse (token, context, chain) {
 			try {
+				const data = Twig.expression.parse.apply(this, [token.variantStack, context]) ? Twig.expression.parse.apply(this, [token.variantStack, context]) : {};
+				const viewIncludes = data.viewIncludes ? data.viewIncludes : '';
+				const viewExcludes = data.viewExcludes ? data.viewExcludes : '';
+
 				const views = view.getViews(config.get('nitro.basePath') + config.get('nitro.viewDirectory'));
 				const markup = ['<ul>'];
 
-				views.forEach((viewItem) => {
+				let filteredViews = views;
+
+				if (viewIncludes !== '') {
+					const viewIncludesArray = viewIncludes.split(';');
+					filteredViews = views.filter((viewItem) => {
+						return viewIncludesArray.some((includeString) => {
+							return viewItem.url.indexOf(includeString) >= 0;
+						});
+					});
+				} else if (viewExcludes !== '') {
+					const viewExcludesArray = viewExcludes.split(';');
+					filteredViews = views.filter((viewItem) => {
+						return viewExcludesArray.every((excludeString) => {
+							return viewItem.url.indexOf(excludeString) === -1;
+						});
+					});
+				}
+
+				filteredViews.forEach((viewItem) => {
 					markup.push(`<li><a href="/${viewItem.url}">${viewItem.name}</a></li>`);
 				});
 
-				markup.push('</ul>');
+				if (filteredViews.length === 0) {
+					markup.push('<li>For this include / exclude term, no pages where found</li>');
+				}
 
 				// return the markup
 				return {

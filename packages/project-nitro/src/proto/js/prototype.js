df6d707635335234ac0212129e3deaec31c20956
ernscht <ernscht@gmail.com>
2019-06-28 17:45:07 +0200

    chore: release 4.7.4

--- packages/project-nitro/src/proto/js/prototype.js
+++ packages/project-nitro/src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 4.7.3');
+console.log('You are using nitro version 4.7.4');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

01c31fe0d8fc1a56079a4c55a31de4d6a1eeebbc
ernscht <ernscht@gmail.com>
2019-09-19 11:26:13 +0200

    chore: release 4.13.1

--- packages/project-nitro-typescript/src/proto/js/prototype.ts
+++ packages/project-nitro-typescript/src/proto/js/prototype.ts
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 4.13.0');
+console.log('You are using nitro version 4.13.1');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

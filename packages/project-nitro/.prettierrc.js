65f1df4d8475e13656f80765d51137eca1ee13e0
ernscht <ernscht@gmail.com>
2019-11-06 18:52:14 +0100

    feat(project): update stylelint and prettier config

--- packages/project-nitro/.prettierrc.js
+++ packages/project-nitro/.prettierrc.js
@@ -1,12 +1,3 @@
 const config = require('@namics/prettier-config');
 
-config.overrides.push(
-	{
-		files: ['*.md'],
-		options: {
-			parser: 'markdown',
-		},
-	},
-);
-
 module.exports = config;

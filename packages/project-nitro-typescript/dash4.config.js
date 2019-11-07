b7fdb0b70d67ac8fa44104eb3bfd90c8528c75ef
ernscht <ernscht@gmail.com>
2019-11-07 16:46:07 +0100

    feat(project): add dashboard #166

---
+++ packages/project-nitro-typescript/dash4.config.js
@@ -0,0 +1,76 @@
+/**
+ *
+ * 	DASH4 configuration
+ *  https://github.com/smollweide/dash4
+ *
+ */
+const { PluginReadme } = require('@dash4/plugin-readme');
+const { PluginNpmScripts } = require('@dash4/plugin-npm-scripts');
+const { PluginActions } = require('@dash4/plugin-actions');
+
+async function getConfig() {
+	return {
+		port: 4444,
+		tabs: [
+			{
+				title: 'START',
+				rows: [
+					[
+						new PluginReadme({ file: 'readme.md' }),
+					],
+					[
+						new PluginNpmScripts({
+							scripts: [
+								{ title: 'Start Development Mode', cmd: 'npm run dev' },
+								{ title: 'Start Production Mode', cmd: 'npm run prod' },
+								{ title: 'Start Cypress Mode', cmd: 'npm run cypress-test' },
+							],
+						}),
+						new PluginNpmScripts({
+							scripts: [
+								{ title: 'Run Linting', cmd: 'npm run lint' },
+								{ title: 'Run Tests', cmd: 'npm run test' },
+								{ title: 'Run Visual Tests', cmd: 'npm run visual-test' },
+								{ title: 'Get Lighthouse Score', cmd: 'npm run lighthouse-test' },
+							],
+						}),
+						new PluginNpmScripts({
+							scripts: [
+								{ title: 'Run Prettier', cmd: 'npm run prettier' },
+								{ title: 'Update Dependencies', cmd: 'npm run update-dependencies' },
+								{ title: 'Clean', cmd: 'npm run clean' },
+							],
+						}),
+					],
+					[
+						new PluginActions({
+							title: 'Links',
+							actions: [
+								{
+									type: 'link',
+									href: 'https://nitro-project-test.netlify.com/',
+									title: 'Proto Server',
+								},
+							],
+						}),
+					],
+				],
+			},
+			{
+				title: 'READMES',
+				rows: [
+					[
+						new PluginReadme({ file: 'project/docs/nitro.md' }),
+					],
+					[
+						new PluginReadme({ file: 'project/docs/nitro-config.md' }),
+						new PluginReadme({ file: 'project/docs/nitro-webpack.md' }),
+						new PluginReadme({ file: 'project/docs/nitro-exporter.md' }),
+					],
+				],
+			},
+		],
+	};
+}
+
+module.exports = getConfig;

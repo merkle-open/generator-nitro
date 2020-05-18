083ab1ff6c0b320c1df8c50ab2537e970f51ca67
ernscht <ernscht@gmail.com>
2020-05-18 21:24:01 +0200

    chore: release 5.2.9

--- packages/project-nitro-typescript/src/proto/js/prototype.ts
+++ packages/project-nitro-typescript/src/proto/js/prototype.ts
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 5.2.8');
+console.log('You are using nitro version 5.2.9');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

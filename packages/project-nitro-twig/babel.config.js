25ea06cbb57709c6c2ac96894706b173f2eb6432
ernscht <ernscht@gmail.com>
2020-05-24 10:40:37 +0200

    feat(webpack): add babel plugin plugin-proposal-class-properties

--- packages/project-nitro-twig/babel.config.js
+++ packages/project-nitro-twig/babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	presets: [[require.resolve('@babel/preset-env'), { useBuiltIns: 'usage', corejs: 3 }]],
 	plugins: [
 		[require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+		require.resolve('@babel/plugin-proposal-class-properties'),
 		require.resolve('@babel/plugin-syntax-dynamic-import'),
 	],
 };

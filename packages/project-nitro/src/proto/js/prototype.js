a1d35baa199b7f7af47f44181363e59c825924da
ernscht <ernscht@gmail.com>
2020-08-10 10:03:32 +0200

    chore: release 5.6.2

--- packages/project-nitro/src/proto/js/prototype.js
+++ packages/project-nitro/src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 5.6.1');
+console.log('You are using nitro version 5.6.2');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

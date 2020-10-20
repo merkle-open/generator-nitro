c89360200dedfd2b5725aec3f3d039d5bb43bdf2
Jan Widmer <jan.widmer@namics.com>
2020-10-20 15:26:57 +0200

    feat(app): enhance hbs pattern helper to have an optional type parameter, enhance tests #183

--- packages/nitro-app/tests/jasmine/templating/patternHbsSpec.js
+++ packages/nitro-app/tests/jasmine/templating/patternHbsSpec.js
@@ -1,21 +1,39 @@
 'use strict';
 
 const config = require('config');
-const helper = require('../../../app/templating/hbs/helpers/pattern');
+const hbs = require('hbs');
 
 describe('Hbs Pattern Helper', () => {
 
 	it('returns an error message if module is unknown', () => {
-		expect(helper('nonexistent')).toMatch(`<p class="nitro-msg nitro-msg--error">Pattern \`nonexistent\` with template file \`nonexistent.${config.get('nitro.viewFileExtension')}\` not found in folder \`nonexistent\`.</p>`);
+		expect(hbs.handlebars.helpers.pattern('nonexistent')).toMatch(`<p class="nitro-msg nitro-msg--error">Pattern \`nonexistent\` with template file \`nonexistent.${config.get('nitro.viewFileExtension')}\` not found in folder \`nonexistent\`.</p>`);
+	});
+
+	it('returns an error message if module with specific type is unknown', () => {
+		expect(hbs.handlebars.helpers.pattern('example', { hash: { template: 'example', type: 'organism' } })).toMatch(`<p class="nitro-msg nitro-msg--error">Pattern \`example\` within pattern type \`organism\` with template file \`example.${config.get('nitro.viewFileExtension')}\` not found in folder \`example\`.</p>`);
+	});
+
+	it('returns an error message if module type is unknown', () => {
+		expect(hbs.handlebars.helpers.pattern('example', { hash: { template: 'example', type: 'nonexistent' } })).toMatch(`<p class="nitro-msg nitro-msg--error">Pattern type \`nonexistent\` not found in pattern config.</p>`);
 	});
 
 	it('returns an object containing a string if module was found', () => {
+		console.log(typeof hbs.handlebars.helpers.pattern('example'))
 		// eslint-disable-next-line
-		expect(helper('example').hasOwnProperty('string')).toBe(true);
+		expect(hbs.handlebars.helpers.pattern('example').hasOwnProperty('string')).toBe(true);
 	});
 
 	it('has a non-empty return value', () => {
-		expect(helper('example').string.length).toBeGreaterThan(0);
+		expect(hbs.handlebars.helpers.pattern('example').string.length).toBeGreaterThan(0);
 	});
 
+	it('returns the first pattern found with the provided name when no type parameter is present', () => {
+		// eslint-disable-next-line
+		expect(hbs.handlebars.helpers.pattern('example')).toMatch('<div>a</div>');
+	});
+
+	it('returns the specific pattern found with the provided name AND type parameter', () => {
+		// eslint-disable-next-line
+		expect(hbs.handlebars.helpers.pattern('example', { hash: { template: 'example', type: 'molecule' } })).toMatch('<div>m</div>');
+	});
 });

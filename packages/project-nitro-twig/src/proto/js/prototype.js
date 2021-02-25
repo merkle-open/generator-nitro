0ca54dce358c87efccf5f6d69062f3f2af3a5211
ernscht <ernscht@gmail.com>
2021-02-25 22:14:54 +0100

    chore: release 5.8.7

--- packages/project-nitro-twig/src/proto/js/prototype.js
+++ packages/project-nitro-twig/src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 5.8.6');
+console.log('You are using nitro version 5.8.7');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

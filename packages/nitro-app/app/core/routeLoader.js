8c508bde66d2838743917c6b07346d7fe8149afb
ernscht <ernscht@gmail.com>
2021-04-04 19:25:15 +0200

    fix(app): unified route sorting across systems

--- packages/nitro-app/app/core/routeLoader.js
+++ packages/nitro-app/app/core/routeLoader.js
@@ -8,7 +8,7 @@ const viewDataPath = `${config.get('nitro.basePath')}project/viewData/`;
 const routers = [];
 
 function readRoutes(routes) {
-	fs.readdirSync(routes).forEach((el) => {
+	fs.readdirSync(routes).sort().forEach((el) => {
 		if (path.extname(el) === '.js') {
 			routers.push(require(routes + path.basename(el, '.js')));
 		}

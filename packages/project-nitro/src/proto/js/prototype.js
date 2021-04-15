6ba323fdd0cfaee3a124fee8697cfda93db38f05
ernscht <ernscht@gmail.com>
2021-04-15 18:04:30 +0200

    chore: release 5.9.1

--- packages/project-nitro/src/proto/js/prototype.js
+++ packages/project-nitro/src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 5.9.0');
+console.log('You are using nitro version 5.9.1');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

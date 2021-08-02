cef64ce00fa7a0cb4a0226aa6fdf30c0bae9205b
ernscht <ernscht@gmail.com>
2021-08-02 17:39:38 +0200

    chore: release 5.10.11

--- packages/project-nitro-typescript/src/proto/js/prototype.ts
+++ packages/project-nitro-typescript/src/proto/js/prototype.ts
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 5.10.10');
+console.log('You are using nitro version 5.10.11');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

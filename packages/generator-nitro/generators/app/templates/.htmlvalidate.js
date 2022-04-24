12a1d3ae7a74a84606d924ea6238033e4e7de98a
ernscht <ernscht@gmail.com>
2022-04-24 19:19:07 +0200

    style: prettify htmlvalidate

--- packages/generator-nitro/generators/app/templates/.htmlvalidate.js
+++ packages/generator-nitro/generators/app/templates/.htmlvalidate.js
@@ -1,20 +1,16 @@
 const config = {
-	'root': true,
-	'extends': [
-		'html-validate:recommended',
-	],
-	'rules': {
+	root: true,
+	extends: ['html-validate:recommended'],
+	rules: {
 		'element-required-attributes': 'warn',
 		'no-inline-style': 'off',
-		'no-raw-characters': [
-			'warn', { 'relaxed': true },
-		],
+		'no-raw-characters': ['warn', { relaxed: true }],
 		'prefer-native-element': 'warn',
 		'svg-focusable': 'off',
 
 		// disable style rules
 		'no-trailing-whitespace': 'off',
 	},
-}
+};
 
 module.exports = config;

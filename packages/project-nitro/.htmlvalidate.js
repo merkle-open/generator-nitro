a5442d1fad6a1fd397d405e0f6d6e006b3a6b789
ernscht <ernscht@gmail.com>
2022-04-27 08:03:36 +0200

    chore: bump html-validate

--- packages/project-nitro/.htmlvalidate.js
+++ packages/project-nitro/.htmlvalidate.js
@@ -3,10 +3,13 @@ const config = {
 	extends: ['html-validate:recommended'],
 	rules: {
 		'element-required-attributes': 'warn',
+		'element-permitted-order': 'warn',
 		'no-inline-style': 'off',
+		'no-implicit-close': 'warn',
 		'no-raw-characters': ['warn', { relaxed: true }],
 		'prefer-native-element': 'warn',
 		'svg-focusable': 'off',
+		'tel-non-breaking': 'off',
 
 		// disable style rules
 		'no-trailing-whitespace': 'off',

54b3475ae72b5af4d67171f1f985e22dd3e758bb
ernscht <ernscht@gmail.com>
2022-06-26 16:11:02 +0200

    chore(project): add html-validate-config

--- packages/generator-nitro/generators/app/templates/.htmlvalidate.js
+++ packages/generator-nitro/generators/app/templates/.htmlvalidate.js
@@ -1,19 +1,3 @@
-const config = {
-	root: true,
-	extends: ['html-validate:recommended'],
-	rules: {
-		'element-required-attributes': 'warn',
-		'element-permitted-order': 'warn',
-		'no-inline-style': 'off',
-		'no-implicit-close': 'warn',
-		'no-raw-characters': ['warn', { relaxed: true }],
-		'prefer-native-element': 'warn',
-		'svg-focusable': 'off',
-		'tel-non-breaking': 'off',
-
-		// disable style rules
-		'no-trailing-whitespace': 'off',
-	},
-};
+const config = require('@merkle-open/html-validate-config');
 
 module.exports = config;

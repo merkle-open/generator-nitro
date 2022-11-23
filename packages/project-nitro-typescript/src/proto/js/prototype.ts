13e11c4e138a9d407de03ac216f9cbf924d8e93f
ernscht <ernscht@gmail.com>
2022-11-23 20:22:57 +0100

    chore: release 7.2.0

--- packages/project-nitro-typescript/src/proto/js/prototype.ts
+++ packages/project-nitro-typescript/src/proto/js/prototype.ts
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 7.1.3');
+console.log('You are using nitro version 7.2.0');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

10274da9d279901db542ef2c416df6c435fccf5d
ernscht <ernscht@gmail.com>
2023-10-26 21:50:49 +0200

    chore: bump deps
    and replace outdated @babel/plugin-proposal-class-properties with @babel/plugin-transform-class-properties

--- packages/generator-nitro/generators/app/templates/babel.config.js
+++ packages/generator-nitro/generators/app/templates/babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	presets: [[require.resolve('@babel/preset-env'), { useBuiltIns: 'usage', corejs: 3 }]],
 	plugins: [
 		[require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-		require.resolve('@babel/plugin-proposal-class-properties'),
 		require.resolve('@babel/plugin-syntax-dynamic-import'),
+		require.resolve('@babel/plugin-transform-class-properties'),
 	],
 };

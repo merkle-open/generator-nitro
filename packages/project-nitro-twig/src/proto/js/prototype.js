4c0f1e1ff64ce10214435ea3ac34ddb96831d3b4
ernscht <ernscht@gmail.com>
2024-02-14 08:32:54 +0100

    chore: release 9.1.0

--- packages/project-nitro-twig/src/proto/js/prototype.js
+++ packages/project-nitro-twig/src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 9.0.3');
+console.log('You are using nitro version 9.1.0');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

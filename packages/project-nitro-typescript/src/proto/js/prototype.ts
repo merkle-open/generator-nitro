0db7398711840792b921b9c89d4ddce77c941d51
ernscht <ernscht@gmail.com>
2025-08-11 10:07:56 +0200

    chore: release 10.0.0

--- packages/project-nitro-typescript/src/proto/js/prototype.ts
+++ packages/project-nitro-typescript/src/proto/js/prototype.ts
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 9.5.0');
+console.log('You are using nitro version 10.0.0');
 console.log('Check for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */

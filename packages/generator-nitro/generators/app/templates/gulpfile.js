977ea53dc2594c9a48bdd0bb0439712dc5f7fa5a
ernscht <ernscht@gmail.com>
2026-04-15 07:51:15 +0200

    feat(gulp): remove outdated gulp-html-validate
    you have to replace script in lint:html

--- packages/generator-nitro/generators/app/templates/gulpfile.js
+++ packages/generator-nitro/generators/app/templates/gulpfile.js
@@ -10,7 +10,6 @@ gulp.task('svg-sprites', getTask('svg-sprites'));
 gulp.task('assets', gulp.parallel('copy-assets', 'minify-images', 'svg-sprites'));
 gulp.task('watch-assets', gulp.series(['assets'], getTask('watch-assets')));
 gulp.task('develop', gulp.series('watch-assets'));
-gulp.task('dump-views', getTask('dump-views'));
-gulp.task('lint-html', gulp.series(['dump-views'], getTask('lint-html')));<% if (options.exporter) { %>
+gulp.task('dump-views', getTask('dump-views'));<% if (options.exporter) { %>
 
 require('@nitro/exporter')(gulp, config);<% } %>
--- packages/project-nitro-typescript/gulpfile.js
+++ packages/project-nitro-typescript/gulpfile.js
@@ -11,6 +11,5 @@ gulp.task('assets', gulp.parallel('copy-assets', 'minify-images', 'svg-sprites')
 gulp.task('watch-assets', gulp.series(['assets'], getTask('watch-assets')));
 gulp.task('develop', gulp.series('watch-assets'));
 gulp.task('dump-views', getTask('dump-views'));
-gulp.task('lint-html', gulp.series(['dump-views'], getTask('lint-html')));
 
 require('@nitro/exporter')(gulp, config);
